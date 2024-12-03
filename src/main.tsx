645860ce17bbe6bb424163ec16a1c372614874ca
Vinicius de Oliveira Jimenez <vinicius.jimenez@itaipuparquetec.org.br>
2024-12-03 08:22:17 -0300

    refactor: rename default files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import { createRoot } from 'react-dom/client'
+
+import App from './app'
+
+const container = document.querySelector('#root')!
+const root = createRoot(container)
+
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>
 )
