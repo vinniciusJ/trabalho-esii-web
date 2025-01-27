2e1596aaff4ee3ed72d2f5d532bc38dac4bf65dc
Vinicius de Oliveira Jimenez <vinicius.jimenez@itaipuparquetec.org.br>
2025-01-27 14:13:38 -0300

    chore: finish docker compose

--- src/service/requests/participants.ts
+++ src/service/requests/participants.ts
@@ -2,19 +2,13 @@ import { EventParticipantForm } from "@/schemas/user"
 import { eventosAPI } from "../eventos"
 
 class EventParticipantService {
-  private apiUrl: string
-
-  constructor(apiUrl: string = `${eventosAPI.defaults.baseURL}/event-participant`) {
-    this.apiUrl = apiUrl
-  }
-
   async fetchTotalParticipants(): Promise<number> {
-    const response = await eventosAPI.get(this.apiUrl)
+    const response = await eventosAPI.get('/event-participant')
     return response.data.length
   }
 
   async registerParticipant(payload: EventParticipantForm): Promise<void> {
-    await eventosAPI.post(this.apiUrl, payload)
+    await eventosAPI.post('/event-participant', payload)
   }
 }
 
